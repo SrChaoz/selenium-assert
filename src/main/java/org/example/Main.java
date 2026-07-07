07c87248eaa774b4ba51aae77d435a2290124fd6
SrChaoz <mateorubencho@gmail.com>
2026-07-07 11:20:13 -0500

    TODOoooooooooo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hola");
+    public static void main(String[] args) {
+        System.out.println("Proyecto Jenkins con JUnit listo.");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    // Función 1
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Función 2
+    public boolean esPar(int numero) {
+        return numero % 2 == 0;
     }
 }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,120 +1,19 @@
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.io.File;
-import java.io.IOException;
-import java.time.Duration;
-import org.junit.jupiter.api.BeforeEach;
-
+import org.example.Main;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class SeleniumTest {
+public class MainTest {
 
-    private static WebDriver driver;
-    private static final String BASE_URL = "https://the-internet.herokuapp.com";
-
-    @BeforeEach
-    public void setUp() {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless=new"); // Forzar modo invisible
-    options.addArguments("--no-sandbox");    // Requerido en entornos de contenedores Linux
-    options.addArguments("--disable-dev-shm-usage"); // Evita problemas de memoria compartida en Linux
-    
-    // Pasa las opciones al constructor
-    this.driver = new ChromeDriver(options); 
-}
-
-    // Método utilitario para cumplir con el requisito de capturas de pantalla
-    public void tomarCaptura(String nombreEjercicio) {
-        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        try {
-            FileUtils.copyFile(srcFile, new File("./capturas/" + nombreEjercicio + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testSumar() {
+        Main app = new Main();
+        assertEquals(5, app.sumar(2, 3), "La suma debería ser 5");
     }
 
     @Test
-    @Order(1)
-    @DisplayName("Ejercicio 1: Verificar el título de la página")
-    public void testVerificarTitulo() {
-        driver.get(BASE_URL + "/");
-        String tituloActual = driver.getTitle();
-
-        assertEquals("The Internet", tituloActual);
-        tomarCaptura("Ejercicio_1_Titulo"); // 
-    }
-
-    @Test
-    @Order(2)
-    @DisplayName("Ejercicio 2: Verificar un encabezado")
-    public void testVerificarEncabezado() {
-        driver.get(BASE_URL + "/");
-        WebElement encabezado = driver.findElement(By.tagName("h1"));
-
-        assertEquals("Welcome to the-internet", encabezado.getText()); //
-        tomarCaptura("Ejercicio_2_Encabezado"); // 
-    }
-
-    @Test
-    @Order(3)
-    @DisplayName("Ejercicio 4: Login incorrecto")
-    public void testLoginIncorrecto() {
-        driver.get(BASE_URL + "/login");
-
-        driver.findElement(By.id("username")).sendKeys("usuario");
-        driver.findElement(By.id("password")).sendKeys("12345");
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-
-        WebElement mensajeError = driver.findElement(By.id("flash"));
-
-        assertTrue(mensajeError.getText().contains("Your username is invalid!"));
-        tomarCaptura("Ejercicio_4_Login_Incorrecto"); // 
-    }
-
-    @Test
-    @Order(4)
-    @DisplayName("Ejercicio 3, 5, 6, 7 y 8: Flujo de Login Exitoso y Logout")
-    public void testFlujoLoginLogout() {
-        // --- EJERCICIO 3: Login Exitoso ---
-        driver.get(BASE_URL + "/login");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-
-        WebElement mensajeExito = driver.findElement(By.id("flash"));
-        assertTrue(mensajeExito.getText().contains("You logged into a secure area!"));
-        tomarCaptura("Ejercicio_3_Login_Exitoso"); // 
-
-        // --- EJERCICIO 5: Verificar URL ---
-        String urlActual = driver.getCurrentUrl();
-        assertTrue(urlActual.contains("secure"));
-        tomarCaptura("Ejercicio_5_Verificar_URL"); // 
-
-        // --- EJERCICIO 6: Verificar botón Logout visible ---
-        WebElement botonLogout = driver.findElement(By.cssSelector("a.button.secondary.radius"));
-        assertTrue(botonLogout.isDisplayed());
-        tomarCaptura("Ejercicio_6_Boton_Visible"); // 
-
-        // --- EJERCICIO 7: Verificar el texto del botón ---
-        assertTrue(botonLogout.getText().contains("Logout"));
-        tomarCaptura("Ejercicio_7_Texto_Boton"); // 
-
-        // --- EJERCICIO 8: Logout ---
-        botonLogout.click();
-        WebElement formularioLogin = driver.findElement(By.id("login"));
-
-        assertNotNull(formularioLogin);
-        tomarCaptura("Ejercicio_8_Logout_Exitoso"); // 
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void testEsPar() {
+        Main app = new Main();
+        assertTrue(app.esPar(4), "El número 4 debería ser par");
+        assertFalse(app.esPar(7), "El número 7 no debería ser par");
     }
 }
