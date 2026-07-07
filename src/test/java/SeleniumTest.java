b7baa0ad710542b54347fe84b2c61412ed967ec5
SrChaoz <mateorubencho@gmail.com>
2026-07-07 11:13:51 -0500

    TODOoooooooooo

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -2,6 +2,7 @@ import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
